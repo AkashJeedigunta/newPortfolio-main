918ae60d387fb534c0eefd48d1e29f7bc23301e2
Akash <blue19489ster@gmail.com>
2025-08-13 15:17:24 -0400

    das

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base:'/newPortfolio-main/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
